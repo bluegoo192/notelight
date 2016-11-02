c5a3a8dbbffa05d1c1d5154c567e54017e791e08
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2016-11-01 17:44:45 -0700

    changed notes to be components

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
-Vue.component('header-test', {
-  template: '<div class="header">\
-  <input class="textbox" v-model="title"></input>\
-  <span>{{ saveStatus }}</span></div>',
+Vue.component('note-page', {
+  template: '<div class="note" draggable="true" @dragstart="dragStart"\
+              @dragend="drop" :style="location">\
+              <div class="header">\
+                <input class="textbox" v-model="title"></input>\
+                <span>{{ saveStatus }}</span>\
+              </div>\
+              <textarea class="content textbox" v-model="content"\
+                placeholder="Enter a note">\
+              </textarea>\
+            </div>',
   data: function () {
     return {
       title: 'New Note2',
-      saveStatus: 'Saved'
-    }
-  }
-})
-
-var app = new Vue({
-  el: '#main',
-  data: {
-    title: 'New Note',
-    content: '',
-    saveStatus: 'Saved',
-    location: {
-      left: '10px',
-      top: '10px'
+      content: '',
+      saveStatus: 'Saved',
+      leftOffset: '0',
+      rightOffset: '0',
+      location: {
+        left: '10px',
+        top: '10px'
+      }
     }
   },
   methods: {
     dragStart: function (event) {
       var style = window.getComputedStyle(event.target, null);
       event.dataTransfer.setData("text/plain",
-      (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' +
-      (parseInt(style.getPropertyValue("top"),10) - event.clientY));
-    },
-    drop: function(event) {
-      var offset = event.dataTransfer.getData("text/plain").split(',');
-      this.location.left = (event.clientX + parseInt(offset[0],10)) + 'px';
-      this.location.top = (event.clientY + parseInt(offset[1],10)) + 'px';
-      return false;
+      (parseInt(style.getPropertyValue("left"),10) - event.screenX) + ',' +
+      (parseInt(style.getPropertyValue("top"),10) - event.screenY));
     },
     save: function () {
       this.saveStatus = 'Saving...';
       this.saveStatus = 'Saved';
+    },
+    drop: function(event) {
+      var offset = event.dataTransfer.getData("text/plain").split(',');
+      this.location.left = (event.screenX + parseInt(offset[0],10)) + 'px';
+      this.location.top = (event.screenY + parseInt(offset[1],10)) + 'px';
+      return false;
     }
   },
   watch: {
@@ -45,6 +46,21 @@ var app = new Vue({
     },
     content: function (noteContent) {
       this.save();
+    }
+  }
+})
+
+
+var app = new Vue({
+  el: '#main',
+  data: {
+    notes: [
+      { text: 'placeholder title' }
+    ]
+  },
+  methods: {
+    makeNote: function() {
+      console.log('stub');
     }
   }
 })
